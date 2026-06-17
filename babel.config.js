3b23b32cb1a2dca9f85bd8e37c56886a54fd7be5
DevDario <dariosilva13222@gmail.com>
2026-06-17 18:15:04 +0100

    feat: add @assets and @illustrations path aliases
    Add path aliases for the assets directory to enable clean imports
    of static assets and illustrations throughout the app.
    
    - Add @assets/* → src/assets/*
    - Add @illustrations/* → src/assets/illustrations/*
    - Wire aliases in both babel module-resolver and tsconfig paths

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,31 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind", unstable_transformImportMeta: true }],
-            "nativewind/babel",
-        ],
-        plugins: [
-            [
-                'module-resolver',
-                {
-                    root: ['./'],
-                    alias: {
-                        '@components': './src/components',
-                        '@screens': './src/features',
-                        '@hooks': './src/hooks',
-                        '@widgets': './src/widgets',
-                        '@store': './src/store',
-                        '@utils': './src/utils',
-                        '@api': './src/services/api',
-                    },
-                },
-            ],
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: [
+      [
+        'babel-preset-expo',
+        { jsxImportSource: 'nativewind', unstable_transformImportMeta: true },
+      ],
+      'nativewind/babel',
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/features',
+            '@hooks': './src/hooks',
+            '@widgets': './src/widgets',
+            '@store': './src/store',
+            '@utils': './src/utils',
+            '@api': './src/services/api',
+            '@assets': './src/assets',
+            '@illustrations': './src/assets/illustrations',
+          },
+        },
+      ],
+    ],
+  };
 };
